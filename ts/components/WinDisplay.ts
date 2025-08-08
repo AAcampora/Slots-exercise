6f8f8ab3a19e3ff3b58cf7252508ae12cd22a093
Alessio <acampora199@gmail.com>
2025-08-08 20:30:11 +0100

    updated win display

--- ts/components/WinDisplay.ts
+++ ts/components/WinDisplay.ts
@@ -1,34 +1,41 @@
-import { Container, Text } from 'pixi.js';
+import { Assets, Container, Sprite, Text } from 'pixi.js';
 import gsap from 'gsap';
+import { GameWinBackground } from '../types/sprite-types';
 
 export class WinDisplay extends Container {
+	private _bg: Sprite;
 	private _text: Text;
 
 	public async init(): Promise<void> {
+		const bg = await Assets.load(GameWinBackground.WIN_BG);
+		this._bg = new Sprite(bg);
+		this._bg.anchor.set(0.5);
+		this.addChild(this._bg);
 		this._text = new Text({
 			text: '0',
 			style: {
 				fontFamily: 'Arial',
-				fontSize: 32,
+				fontSize: 45,
 				fill: '#ffffff',
 				stroke: { color: '#000000', width: 4 },
 			},
 		});
 		this._text.alpha = 0;
+		this._bg.alpha = 0;
 		this.addChild(this._text);
 		this._text.anchor.set(0.5);
 	}
 
 	public showWin(cash: number): void {
+		gsap.killTweensOf([this._text, this._bg]);
 		this._text.text = '1% $'.replace('1%', cash.toString());
 		this._text.position.y = 0;
-		gsap.to(this._text, {
-			duration: 0.2,
-			alpha: 1,
-		});
-		gsap.to(this._text, {
+		this._bg.position.y = 0;
+		this._text.alpha = 0;
+		this._bg.alpha = 0;
+		gsap.to([this._text, this._bg], {
 			duration: 0.5,
-			scale: 2,
+			alpha: 1,
 			onComplete: () => {
 				this.fadeoutWin();
 			},
@@ -36,7 +43,7 @@ export class WinDisplay extends Container {
 	}
 
 	private fadeoutWin(): void {
-		gsap.to(this._text, {
+		gsap.to([this._text, this._bg], {
 			y: -200,
 			duration: 1,
 			alpha: 0,
